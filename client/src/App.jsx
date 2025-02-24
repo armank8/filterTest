7252736913d89c8e8c742e5079010ab8cd0a0f63
Arman-Kabir <armankabir8@gmail.com>
2025-02-24 11:36:10 +0600

    client server installations

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h3 className='text-3xl'>hello</h3>
+      <button className="btn btn-primary">Primary</button>
+    </div>
+  )
+}
+
+export default App
